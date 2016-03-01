b04ff15899d66c6d8ab3ffb6a732ac5968ad6f05
jordan <jordanalex.martinez@gmail.com>
2016-03-01 12:45:32 -0800

    Use a more linear way of showing how to use the Git object.
    Note: File is used instead of Path because of a bug I found. On Linux Mint with 1.8.0_74, trying to create a relative Path using Path#relativize, Path#subPath, or Path#getName will return a path whose absolute string is: "path/to/present/working/directory/base_path/relative_path" instead of "relative_path". When this gets resolved for Linux, I'll update the code to use Path where applicable.

--- jgitfx/src/test/groovy/com/jgitfx/jgitfx/GitActionsSpec.groovy
+++ jgitfx/src/test/groovy/com/jgitfx/jgitfx/GitActionsSpec.groovy
@@ -1,66 +1,110 @@
 package com.jgitfx.jgitfx
 
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.api.Status
+import org.eclipse.jgit.api.errors.RefNotFoundException
+import org.eclipse.jgit.lib.PersonIdent
+import org.eclipse.jgit.lib.Ref
+import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Stepwise
 
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.Paths
-
-
-/**
- * Created by jordan on 2/11/16.
- */
 @Stepwise
 class GitActionsSpec extends Specification {
 
-    def "CreateRepoInParentDir creates a new '.git' repo in that directory"() {
-        given: "Some parent directory..."
-        Path parentDirPath = Paths.get(".")
-        println "\t\t====Parent Dir Path: ${parentDirPath.toAbsolutePath().toString()}====="
-        File parentDir = parentDirPath.toFile()
+    @Shared
+    File rootDir
 
-        and: "...that does not have a git repository"
-        Path gitRepoDirPath = parentDirPath.resolve(".git")
-        Files.deleteIfExists(gitRepoDirPath)
+    @Shared
+    File file
 
-        when: "Method #createRepoIn is called"
-        Git git = GitActions.createRepoIn(parentDir)
+    @Shared
+    String relativePath
 
-        then: "a new repo is created"
-        File gitRepoDir = git.getRepository().getDirectory()
-        gitRepoDir.exists()
+    @Shared
+    Git git
 
-        and: "the repo ends with '.git'"
-        gitRepoDir.name.endsWith(".git")
+    @Shared
+    PersonIdent fakeAuthor
 
-        and: "it is within that parent directory."
-        gitRepoDir.getParentFile() == parentDir
+    def setupSpec() {
+        rootDir = new File(System.getProperty("user.home"), "TestRepoDir")
+        rootDir.mkdir()
 
-        cleanup: "close but do not delete the repo so it can be used for next test"
-        git.repository.close()
-        git.close()
+        file = new File(rootDir, "file.txt")
+        file.createNewFile()
+
+        // get relative file path
+        String relative = file.path - file.parent
+
+        // still need to remove the file separator
+        // "/file.txt" --> "file.txt"
+        relativePath = relative.substring(1)
+
+        fakeAuthor = new PersonIdent("author", "address@domain.com")
+
+        // create a new repository
+        git = Git.init().setDirectory(rootDir).call()
     }
 
-    def "OpenRepo opens the previously saved '.git' repo"() {
-        given: "Some repository..."
-        Path gitRepoDirPath = Paths.get(".").resolve(".git")
-        File gitRepoDir = gitRepoDirPath.toFile()
+    def "new repository has no branches and attempting to create one fails"() {
+        given: "branch list"
+        List<Ref> branchList = git.branchList().call()
 
-        expect: "...that exists"
-        Files.exists(gitRepoDirPath)
+        expect: "branch list to be empty"
+        branchList.isEmpty()
 
-        when: "the repositor is opened"
-        Git git = GitActions.openRepo(gitRepoDir)
+        when: "attempt to create a 'master' branch"
+        git.branchCreate().setName("master").call()
 
-        then: "the repository actually exists"
-        git.repository.getDirectory() == gitRepoDir
-
-        // when other tests are implemented, remove this cleanup so others can use the sample Git repository
-        cleanup: "close and delete the repo"
-        git.repository.close()
-        git.close()
-        gitRepoDir.deleteDir()
+        then: "that throws a RefNotFoundException: Cannot resolve HEAD"
+        thrown(RefNotFoundException)
     }
+
+    def "Making initial commit will indirectly create a 'master' branch"() {
+        setup: "when status is called when directory has a new file and repo has no commits"
+        Status status = git.status().call()
+
+        expect: "file is listed as 'untracked'"
+        status.untracked[0] == relativePath
+
+        when: "file is added"
+        git.add().addFilepattern(relativePath).call()
+
+        and: "status is requested"
+        status = git.status().call()
+
+        then: "file is listed as 'new'"
+        status.untracked.isEmpty()
+        status.added[0] == relativePath
+
+        when: "file is committed"
+        git.commit()
+                .setAuthor(fakeAuthor)
+                .setMessage("initial commit: add main file to repository")
+                .call()
+
+        and: "status is requested"
+        status = git.status().call()
+
+        then: "there are no uncommitted changes"
+        !status.hasUncommittedChanges()
+        status.added.isEmpty()
+
+        when: "branch list is requested"
+        List<Ref> branchList = git.branchList().call()
+
+        then: "a 'master' branch has been created indirectly"
+        branchList.size() == 1
+        branchList[0].name.endsWith("master") // name == "refs/heads/master"
+    }
+
+    def cleanupSpec() {
+        // clean up memory
+        git.close()
+
+        // delete test repo dir
+        rootDir.deleteDir()
+    }
+
 }
