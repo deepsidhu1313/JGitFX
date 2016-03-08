b84d8539d86fb174058e3d991814eeb8bd561f31
jordan <jordanalex.martinez@gmail.com>
2016-03-08 10:55:28 -0800

    Add an AddMenuItem for quickly integrating into a ContextMenu that is shown via a right-click on a TreeView's TreeCell.

---
+++ jgitfx/src/main/java/com/jgitfx/jgitfx/menus/AddMenuItem.java
@@ -0,0 +1,41 @@
+package com.jgitfx.jgitfx.menus;
+
+import javafx.scene.control.MenuItem;
+import org.eclipse.jgit.api.AddCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.reactfx.value.Val;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+/**
+ * A {@link MenuItem} that will add the untracked file(s) to the git repository.
+ * <p>Note: this object is intended to be added to a {@link javafx.scene.control.ContextMenu} that is displayed
+ * when a user right-clicks a row (presumably representing some file) in a TreeView; it is not intended to be
+ * added to a {@link javafx.scene.control.Menu} though that is still possible.
+ */
+public abstract class AddMenuItem extends MenuItem {
+
+    /**
+     *
+     * @param git
+     * @param filePatternGetter the list of the relative path(s) from the root directory of the files to add/stage.
+     */
+    public AddMenuItem(Val<Git> git, Supplier<List<String>> filePatternGetter) {
+        super();
+        setText("Add item...");
+        setOnAction(ae -> {
+            try {
+                AddCommand addCmd = git.getOrThrow().add();
+                // insure add command will add newly staged files
+                addCmd.setUpdate(false);
+                filePatternGetter.get().forEach(addCmd::addFilepattern);
+                addCmd.call();
+            } catch (GitAPIException e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
+}
