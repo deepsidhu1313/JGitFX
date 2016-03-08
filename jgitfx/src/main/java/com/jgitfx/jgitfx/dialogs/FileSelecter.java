484fc395769a60eaf88c35d60301171268fe2438
jordan <jordanalex.martinez@gmail.com>
2016-03-08 09:21:34 -0800

    Added javadoc to FileSelector

--- jgitfx/src/main/java/com/jgitfx/jgitfx/dialogs/FileSelecter.java
+++ jgitfx/src/main/java/com/jgitfx/jgitfx/dialogs/FileSelecter.java
@@ -5,12 +5,23 @@ import org.eclipse.jgit.api.Status;
 
 import java.util.List;
 
-
+/**
+ * An interface for a custom GUI component that displays all the files on which
+ * an action can be applied and which of those files  the user has selected for
+ * an action to actually be applied.
+ */
 public interface FileSelecter {
 
+    /**
+     *
+     * @return true at least one file has been selected
+     */
     boolean hasSelectedFiles();
     ObservableBooleanValue hasSelectedFilesProperty();
 
+    /**
+     * Refreshes the GUI component that displays the files when {@link Status#hasUncommittedChanges()}} returns true
+     */
     void refreshTree(Status status);
 
     List<String> getSelectedFiles();
